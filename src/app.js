aae475442a3cd2ecdd089fa83b36c30cc71e3571
fatihhozturk <fatihozt3@hotmail.com>
2023-08-28 12:41:20 +0300

    28 Ağustos Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,25 @@
-console.log("Merhaba Kodlama.io")
+console.log("Merhaba kodlama.io")
 
-//JS type safe değildir
-let dolarBugun = 9.30
+let dolarDun = 15.20
+// dolarDun = "Ankara"
+let dolarBugun = 15.16
 
-let dolarDun = 9.20
-dolarDun = "9.20"
 {
-    let dolarDun = 9.10
+    let dolarDun = 31
 }
 
 console.log(dolarDun)
 
 const euroDun = 11.2
-//euroDun = 11
+// euroDun = 11  bunu yapamıyoruz
 
-console.log(euroDun)
+let konutKredileri = ["konut kredisi","taşıt kredisi","emlak kredisi","özel konut kredisi"]
+console.log(konutKredileri)
 
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
 
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+for(let i=0 ; i<konutKredileri.length ; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>") 
 }
 console.log("</ul>")
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
